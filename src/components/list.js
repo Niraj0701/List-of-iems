23a7f1514a64e4f1bf3f90762d6a1e4fe27ed362
Niraj Pachpande <Niraj@Nirajs-MacBook-Pro.local>
2019-09-14 20:00:36 +0200

    Updates for OnClick Methode

--- src/components/list.js
+++ src/components/list.js
@@ -6,14 +6,14 @@ export default class ListPage extends React.Component {
    constructor(props) {
       super(props);
       console.log('Component init - ListPage: ')
-      this.ckickHere = this.clickHere.bind(this);
+      this.clickHere = this.clickHere.bind(this);
    }
 
    componentDidMount() {
       
    }
    clickHere() {
-      this.props.clickedButton('ok')
+      this.props.getListOfEquipments()
       
    }
 
@@ -21,12 +21,12 @@ export default class ListPage extends React.Component {
      return (
       <section>
          <h1>From List Component: - {this.props.name}</h1>
-         <button onClick={() => this.clickHere()}> {this.props.name} </button>
+         <button onClick={this.clickHere}> {this.props.name} </button>
       </section>);
    }
  }
 
  ListPage.propTypes = {
     name: PropTypes.string,
-    clickedButton: PropTypes.func
+    getListOfEquipments: PropTypes.func
  }
--- src/containers/list/list.container.js
+++ src/containers/list/list.container.js
@@ -4,8 +4,8 @@ import ListPage from '../../components/list';
 const  mapStateToProps = () => ({name: 'niraj'});
 
 const mapDispatchToProps = (dispatch) => ({
-   clickedButton: (test) => {
-      console.log('mapDispatchToProps hit', test);
+   getListOfEquipments: () => {
+      console.log('mapDispatchToProps hit');
    }
 });
 
